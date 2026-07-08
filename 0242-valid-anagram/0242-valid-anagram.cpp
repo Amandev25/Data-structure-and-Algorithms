eb8dbc36e0e627e9659ee077c0d1bda260f8adcb
Aman Yadav <amanyadav.dev25@gmail.com>
2026-07-09 01:29:03 +0530

    Time: 0 ms (100%), Space: 10 MB (11.38%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) 
+    {
+      unordered_map<char ,int>mp ; 
+      for( char it : s )
+      {
+        mp[it ]++ ; 
+      }   
+      for( char it : t )
+      {
+        mp[it]-- ; 
+      }
+      for( auto it : mp )
+      {
+        if( it.second < 0 || it.second > 0 ) return false ; 
+      }
+      return true ; 
+      
+
+    }
+};
