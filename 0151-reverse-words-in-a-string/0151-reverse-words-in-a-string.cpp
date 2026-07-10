0e35675db3b78fb78fac30733f054ed9c1f23ebe
Aman Yadav <amanyadav.dev25@gmail.com>
2026-07-11 00:06:11 +0530

    Time: 0 ms (100%), Space: 10.1 MB (69.93%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,45 +1,38 @@
 class Solution {
 public:
-    string reverseWords(string s) {
-        vector<string> words;
-        int n = s.size();
-        int i = 0;
-
-        while (i < n) {
-
-            // Skip spaces
-            while (i < n && s[i] == ' ') {
-                i++;
-            }
-
-            if (i >= n) break;
-
-            // Find the end of the word
-            int j = i;
-
-            while (j < n && s[j] != ' ') {
-                j++;
-            }
-
-            // Store the word
-            words.push_back(s.substr(i, j - i));
-
-            // Move i to the next position
-            i = j;
+    string reverseWords(string s) 
+    {
+      reverse( s.begin() , s.end() ) ; 
+      int n = s.size() ; 
+      int left = 0 , right ; 
+      string ans ="" ; 
+      while( left < n )
+      {
+        while( left < n && s[left] == ' ')
+        {
+            left++ ; 
         }
 
-        reverse(words.begin(), words.end());
-
-        string ans;
-
-        for (int i = 0; i < words.size(); i++) {
-            if (i > 0) {
-                ans += " ";
-            }
-
-            ans += words[i];
+        if( left >= n ) break ; 
+        right = left ; 
+        while( right < n && s[right] != ' ')
+        {
+            right++ ;
         }
 
-        return ans;
+        reverse( s.begin()+left , s.begin()+right ) ; 
+
+        if( !ans.empty() )
+        {
+            ans += ' ' ; 
+        }
+        ans += s.substr( left , right - left ) ; 
+
+        left = right ; 
+
+      }
+
+      return ans ; 
+
     }
 };
